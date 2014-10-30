5d6fc8aa1facc1a1ae5a378156599044e4e3a9c1
Krzysztof Kula <krzychukula@gmail.com>
2014-10-30 08:38:52 +0100

    fix return value and test it

--- wiki.go
+++ wiki.go
@@ -21,5 +21,12 @@ func loadPage(title string) (*Page, error) {
 	if err != nil {
 		return nil, err
 	}
-	return &Page{Title: title, Body: body}
+	return &Page{Title: title, Body: body}, nil
+}
+
+func main() {
+	p1 := &Page{Title: "TestPage", Body: []byte("This is a sample Page.")}
+	p1.save()
+	p2, _ := loadPage("TestPage")
+	fmt.Println(string(p2.Body))
 }
